2f296cf9350e61d79953b283b27943d0c95a681d
AlexanderThaep <odomsathshia_tep@brown.edu>
2023-12-24 14:59:39 -0800

    WIP

---
+++ testAlgo2.py
@@ -0,0 +1,17 @@
+import numpy as np
+from skimage import io
+
+def gaussian_kernel(size, sigma=1):
+    size = int(size) // 2
+    x, y = np.mgrid[-size:size+1, -size:size+1]
+    normal = 1 / (2.0 * np.pi * sigma**2)
+    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
+    return g
+
+original = io.imread(
+    fname='balls.jpg', 
+    as_gray=True)
+
+dotCloud = []
+
+print(original)
