dd9e0e1a184cb237d59cdd5760ae325415519f78
AlexanderThaep <odomsathshia_tep@brown.edu>
2023-12-28 14:12:53 -0800

    Blurring step

--- testAlgo2.py
+++ testAlgo2.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 from skimage import io
 
@@ -8,10 +9,18 @@ def gaussian_kernel(size, sigma=1):
     g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
     return g
 
-original = io.imread(
-    fname='balls.jpg', 
-    as_gray=True)
+def applyKernel(matrix, kernel):
+    return cv2.filter2D(matrix, -1, kernel)
+
+original = cv2.imread(
+    filename='balls.jpg',
+    flags=cv2.IMREAD_GRAYSCALE
+    )
+
+g = gaussian_kernel(5)
+copy = applyKernel(matrix=original, kernel=g)
+
+io.imshow(copy.astype(np.uint8))
+io.show()
 
 dotCloud = []
-
-print(original)
