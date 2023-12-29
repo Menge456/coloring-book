fc8e89eaf90bd1711fe239b87d86f31452551946
Mengistie Hailemariam <menge456123@gmail.com>
2023-12-29 14:26:58 -0500

    beginning shift towards typescript

---
+++ backend.ts
@@ -0,0 +1,25 @@
+
+import { PythonShell } from 'python-shell';
+
+function changeImage(event){
+    /*This isn't working how i'm expecting it to rn, but it's working as a way for the user to download their file
+    const theFiles = document.getElementById("imageReceiver").files;
+    print(theFiles[0]);
+    *
+
+    const theFiles = document.getElementById("imageReceiver").files;
+
+    
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+          //scriptPath: 'path/to/my/scripts', //If you are having python_test.py script in same folder, then it's optional.
+        args: [theFiles[0].name]
+    };
+    PythonShell.run('testAlgo.py', options).then(messages=>{
+        console.log('finished');
+      });
+    document.getElementById("downloadSpot") = "<a href= \"output.png\"> "
+    */
+}
+
--- testAlgo.py
+++ testAlgo.py
@@ -1,33 +1,34 @@
 import numpy as np
 from skimage import io
 from sklearn.cluster import KMeans
+import sys
 
-def imageChanger(original):
-    original = io.imread(original)
-    n_colors = 5
 
-    arr = original.reshape((-1, 3))
-    kmeans = KMeans(n_clusters=n_colors, random_state=42).fit(arr)
-    labels = kmeans.labels_
-    centers = kmeans.cluster_centers_
-    less_colors = centers[labels].reshape(original.shape).astype('uint8')
-    def compColors(img1, img2):
-        for i in range(len(img1)):
-            if(img1[i] != img2[i]):
-                return False
-        return True
+original = io.imread(sys.argv[1])
+n_colors = 5
 
-    res = np.copy(original)
-    for i in range(0, less_colors.shape[0]):
-        for j in range(0, less_colors.shape[1]):
-            for x in range(i-2, i+2):
-                for y in range(j - 2, j+2):
-                    if(not (x < 0 or x >= less_colors.shape[0] or y < 0 or y >= less_colors.shape[1])):
-                        if(not compColors(less_colors[x][y], less_colors[i][j])):
-                            res[i][j] = 0
-                            x = i + 2
-                            y = j+2 
-                        else:
-                            res[i][j] = 255
-    return res.astype(np.uint8)
+arr = original.reshape((-1, 3))
+kmeans = KMeans(n_clusters=n_colors, random_state=42).fit(arr)
+labels = kmeans.labels_
+centers = kmeans.cluster_centers_
+less_colors = centers[labels].reshape(original.shape).astype('uint8')
+def compColors(img1, img2):
+    for i in range(len(img1)):
+        if(img1[i] != img2[i]):
+            return False
+    return True
+
+res = np.copy(original)
+for i in range(0, less_colors.shape[0]):
+    for j in range(0, less_colors.shape[1]):
+        for x in range(i-2, i+2):
+            for y in range(j - 2, j+2):
+                if(not (x < 0 or x >= less_colors.shape[0] or y < 0 or y >= less_colors.shape[1])):
+                    if(not compColors(less_colors[x][y], less_colors[i][j])):
+                        res[i][j] = 0
+                        x = i + 2
+                        y = j+2 
+                    else:
+                        res[i][j] = 255
+io.imsave("output.jpg", res.astype(np.uint8))
 
