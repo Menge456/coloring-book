8910a266e04fb2cd3d3910aedb30012bea68b573
Mengistie Hailemariam <menge456123@gmail.com>
2024-01-02 19:22:01 -0500

    more minor css stuff

--- new-coloring-book/renderer.js
+++ new-coloring-book/renderer.js
@@ -10,7 +10,16 @@ function func() {
   //console.log(theFiles[0].name)
   let newImg =  window.versions.changeImage();
   console.log(newImg);
-  document.getElementById("putHere").innerHTML = "<a href=\'output.jpg\' download> yourColoringBookPage! </a>"
+  document.getElementById("putHere").innerHTML = "<a href=\'output.jpg\' download> Here's your image! </a>"
+}
+
+/*
+  lighten
+  input: background colors where the backgroundColor returns something of the form rgb(x, y, z) where x y and z are numbers
+  output: increased values for x, y, and z by a number (which will be determined through testing), if the numbers are already >= 255, return 255
+*/
+function lighten(theId){
+ theId.classList.toggle("active")
 }
 
 
